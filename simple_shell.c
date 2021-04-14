bb6fca3e42df8e13567508441f422e1e7236df7c
Ignacio Chitnisky <elinmortal@gmail.com>
2021-04-14 05:14:19 +0000

    changing token0 to splitted 0 line 57

--- simple_shell.c
+++ simple_shell.c
@@ -51,11 +51,12 @@ int main(int ac __attribute__((unused)), char **av, char **env)
 		write(1, "#cisfun$ ", 9);
 		n = getline(&input, &size, stdin);
 
-		if (input[0] == '\n')
-			break;
-
+	
 		input[n - 1] = ' ';
 		splitted = _split(input, " \n\t\r");
+			if (splitted[0] == NULL)
+			continue;
+
 		switch (core(input, splitted, lines, env, av))
 		{
 			case 0:
